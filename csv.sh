f1cc61fdf49d075fcc62f6a3da956c5a3f0f84b2
7erry <twal7ers@gmail.com>
2019-05-29 21:06:17 -0400

    added comment illustrating a sample execution using the provided config file and data.csv

--- csv.sh
+++ csv.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+# sample run: ./csv.sh -hc=src/main/resources/hazelcast-client.xml -mn=employees -if=name -v src/main/resources/data.csv
 java -jar target/csvToIMDG-1.0-SNAPSHOT-jar-with-dependencies.jar $1 $2 $3 $4 $5 $6 $7 $8
